940028558c289a2a9e048cb22b127ae3063d768c
CawaTrueYT5 <hlushkosashas@gmail.com>
2025-05-10 18:01:27 +0300

    News update (sections 'Новини' and 'Оголошення' now work)

--- news/models.py
+++ news/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 class Novation(models.Model):
     title = models.CharField(max_length=100)
+    content = models.TextField()
 
     def __str__(self):
         return self.title
 
 class Announ(models.Model):
     title = models.CharField(max_length=100)
+    content = models.TextField()
 
     def __str__(self):
         return self.title
