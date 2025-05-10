254066a74ce4ddddb04ecd5e3a42b33f02958bee
viverna11 <pash110707@gmail.com>
2025-05-10 16:46:48 +0300

    i working

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
    path("login/", views.login_view, name='login'),
    path("logout/", views.logout_view, name='logout'),
    path("register/", views.register, name='register'),
-   path("profile/", views.profile, name='profile'),
+   path("portfolio/", views.portfolio, name='portfolio'),
    path("novations/", views.novations, name='novations'),
    path("announs/", views.announs, name='announs'),
+
 ]
