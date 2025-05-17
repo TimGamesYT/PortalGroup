72f68b40e732d39d61b6df5ea5b22f07460d243e
CawaTrueYT5 <hlushkosashas@gmail.com>
2025-05-17 18:23:09 +0300

    Section news deleted and added to the main page, some cosmetic changes

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("novations/", views.NovationsListView.as_view(), name='novations'),
+   path("", views.NovationsListView.as_view(), name='novations'),
    path("announs/", views.AnnounsListView.as_view(), name='announs'),
 ]
